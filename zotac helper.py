13bfe1c9491ae1473c1be616f3ff4bfcffa6a698
Gymnast544 <oberoiarjun2009@gmail.com>
2021-03-28 12:22:22 -0400

    Added the main program

---
+++ zotac helper.py
@@ -0,0 +1,22 @@
+import webbrowser
+
+
+def open_tab(url):
+    webbrowser.get("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s").open_new_tab(url)
+
+steps = ["1: Login", "2: ATC", "3: Paypal start", "4: Paypal review"]
+urls = ["https://www.zotacstore.com/us/customer/account/login/", "https://www.zotacstore.com/us/graphics-cards/geforce-rtx-30-series?limit=24", "https://store.zotac.com/paypal/express/start/", "https://store.zotac.com/paypal/express/review/"]
+amounts = [15, 20, 20, 50]
+
+while True:
+    print("\n\n\n")
+    print("Select a step")
+    for step in steps:
+        print(step)
+    try:
+        stepindex = int(input())
+        print("You selected step "+steps[stepindex-1])
+        for i in range(amounts[stepindex-1]):
+            open_tab(urls[stepindex-1])
+    except:
+        print("Enter a valid integer that corresponds to a step")
